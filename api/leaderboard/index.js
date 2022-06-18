5a12a6530f94b5d34e4775c261f025023648da70
Ben Rimbey <ben.rimbey@gmail.com>
2022-06-18 13:23:22 -0700

    [ux][preview] - adding preview modal

--- api/leaderboard/index.js
+++ api/leaderboard/index.js
@@ -23,7 +23,7 @@ const stubData = [
   {
     "id": "ccdf233e4ef583c3d07e2e68da09abcsdfq3r9",
     data: {
-      "salt": 81.8462,
+      "salt": 79,
       "commanders": ["Ragaskankavan"],
       "author": "grumbledore",
       "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
@@ -39,7 +39,7 @@ const stubData = [
   {
     "id": "ccdf233e4ef583c3d07e2e68da09abc9ds123123f",
     data: {
-      "salt": 72.6154,
+      "salt": 59,
       "commanders": ["Ragaskankavan"],
       "author": "grumbledore",
       "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
@@ -53,9 +53,137 @@ const stubData = [
     }
   },
   {
-    "id": "ccdf233e4ef583c3d07e2e68da09abc9sdfd",
+    "id": "25fc173802ccc249aad3edc0f7b27714",
     data: {
-      "salt": 52.8813,
+      "salt": 39,
+      "commanders": ["TESTENHOFFER"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "TOTALLY A REAL DECK YALL",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/kchtLi5Kmk2K94zHoNlXMA",
+      "timesIndexed": "",
+      "id": "25fc173802ccc249aad3edc0f7b27714"
+    }
+  },
+  {
+    "id": "25fc173802ccc249aad3edc0f7b2771425fc173802ccc249aad3edc0f7b2771425fc173802ccc249aad3edc0f7b27714",
+    data: {
+      "salt": 29,
+      "commanders": ["TESTENHOFFER"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "TOTALLY A REAL DECK YALL",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/kchtLi5Kmk2K94zHoNlXMA",
+      "timesIndexed": "",
+      "id": "25fc173802ccc249aad3edc0f7b27714"
+    }
+  },
+  {
+    "id": "ww3sdkjfsfkj",
+    data: {
+      "salt": 9,
+      "commanders": ["Ragaskankavan", "Sextuples"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "Rakdos, Lord of Death Punchies",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/kchtLi5Kmk2K94zHoNlXMA",
+      "timesIndexed": "",
+      "id": "25fc173802ccc249aad3edc0f7b27714"
+    }
+  },
+  {
+    "id": "234324324",
+    data: {
+        "salt": 111.3378,
+        "author": "bobkozilek",
+        "commanders": ["Ragaskankavan"],
+        "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-78719-65c4fa40-f937-4b65-adac-f402893cefd8",
+        "source": "moxfield",
+        "title": "zur3",
+        "authorProfileUrl": "https://www.moxfield.com/users/bobkozilek",
+        "dateLastIndexed": "",
+        "url": "https://www.moxfield.com/decks/S3_OUj3m0UatiYy2VMOM8Q",
+        "timesIndexed": "",
+        "id": "ccdf233e4ef583c3d07e2e68da09abc9"
+    },
+  },
+  {
+    "id": "546456456",
+    data: {
+      "salt": 79,
+      "commanders": ["Ragaskankavan"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "Sen Triplets, Friendship Ruination Committee",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/sHgMapORlEmNzZEGb_nrRw",
+      "timesIndexed": "",
+      "id": "c7940c28156dbde0339dc204d94e7c12"
+    }
+  },
+  {
+    "id": "7567",
+    data: {
+      "salt": 59,
+      "commanders": ["Ragaskankavan"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "Ragavan, the Wee Arsehole",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/ba2V0uVjzUmGKGe958nmcA",
+      "timesIndexed": "",
+      "id": "e910a61ffbf537b7c3d9164850701c84"
+    }
+  },
+  {
+    "id": "8",
+    data: {
+      "salt": 39,
+      "commanders": ["TESTENHOFFER"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "TOTALLY A REAL DECK YALL",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/kchtLi5Kmk2K94zHoNlXMA",
+      "timesIndexed": "",
+      "id": "25fc173802ccc249aad3edc0f7b27714"
+    }
+  },
+  {
+    "id": "09",
+    data: {
+      "salt": 29,
+      "commanders": ["TESTENHOFFER"],
+      "author": "grumbledore",
+      "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
+      "source": "moxfield",
+      "title": "TOTALLY A REAL DECK YALL",
+      "authorProfileUrl": "https://www.moxfield.com/users/grumbledore",
+      "dateLastIndexed": "",
+      "url": "https://www.moxfield.com/decks/kchtLi5Kmk2K94zHoNlXMA",
+      "timesIndexed": "",
+      "id": "25fc173802ccc249aad3edc0f7b27714"
+    }
+  },
+  {
+    "id": "sdkjrhw3",
+    data: {
+      "salt": 9,
       "commanders": ["Ragaskankavan", "Sextuples"],
       "author": "grumbledore",
       "authorAvatarUrl": "https://assets.moxfield.net/profile/profile-12325-84750aee-56a3-4a32-afd4-e393529e2622",
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,32 +1,98 @@
 import React from 'react';
-import {Text, Flex} from '@adobe/react-spectrum'
+import {Text, Flex, DialogContainer, Divider, Well, ActionButton} from '@adobe/react-spectrum'
 import PropTypes from "prop-types";
 import './Preview.css';
+import LinkOut from '@spectrum-icons/workflow/LinkOut';
+import Close from '@spectrum-icons/workflow/Close';
+
 
 export class Preview extends React.Component {
 
     static propTypes = {
         deck: PropTypes.object,
+        onDismiss: PropTypes.func,
+      }
+
+      handleAuthorLinkPress = (evn) => {
+        console.log(`found :: ${this?.props?.deck?.authorProfileUrl}`)
+        window.location.href = this?.props?.deck?.authorProfileUrl;
+      }
+
+      handleDeckLinkPress = (evn) => {
+        console.log(`found :: ${this?.props?.deck?.url}`)
+        window.location.href = this?.props?.deck?.url;
+      }
+
+      getSaltRating = (val) => {
+        if (val < 10) {
+            return "Grade F: WHERE IS THE SALT?!";
+        } else if (val < 30) {
+            return "Grade D: Under seasoned";
+        } else if (val < 40) {
+            return "Grade C: Pass the salt";
+        } else if (val < 60) {
+            return "Grade B: Well seasoned";
+        } else if (val < 80) {
+            return "Grade A: Completely balanced!";
+        }
+
+        return "Grade A+: PERFECTION";
       }
 
       render() {
-        const avatarUrl = this?.props?.deck?.data?.authorAvatarUrl;
-        // const authorUrl = this?.props?.deck?.data?.authorProfileUrl;
-        const title = this?.props?.deck?.data?.title;
-        const salt = this?.props?.deck?.data?.salt;
-        const author = this?.props?.deck?.data?.author;
+        const avatarUrl = this?.props?.deck?.authorAvatarUrl;
+        const title = this?.props?.deck?.title;
+        const salt = this?.props?.deck?.salt;
+        const author = this?.props?.deck?.author;
+        const commanders = this?.props?.deck?.commanders?.toString().replace(`,`, `\n`);
+
+        console.log(`TITLE ${JSON.stringify(this?.props?.deck)}`)
 
         return (
-            <Flex direction="column">
-                <Flex direction="row">
-                    <img src={avatarUrl} height="100px" alt="avatar" />
-                    <Flex direction="column">
-                        <Text UNSAFE_className="TitleText">{title}</Text>
-                        <Text UNSAFE_className='AuthorText'>{author}</Text>
-                    </Flex>
-                </Flex>
-                Score: {salt}
-            </Flex>
+            <DialogContainer type='modal' isDismissable onDismiss={this?.props?.onDismiss}>
+                {/* <Dialog 
+                    width="480px"> */}
+                    <div className='PreviewContainer' width="100%">
+                        <Flex direction="column" width="size-4000">
+                            <ActionButton 
+                                type="reset"
+                                alignSelf="flex-end"
+                                onPress={this?.props?.onDismiss}><Close /></ActionButton>
+                            <Flex direction="row" gap="size-130" marginTop="10px">
+                                <Flex direction="column">
+                                    <img src={avatarUrl} width="100" alt="avatar" />
+                                    <Text UNSAFE_className="AuthorText">{author}</Text>
+                                </Flex>
+                                <Flex direction="column" width="100%">
+                                    <Text UNSAFE_className="TitleText">{title}</Text>
+                                    
+                                    <Text UNSAFE_className="CommanderText">Commander(s):</Text>
+                                    <div style={{height: '100%'}} />
+                                    <Text UNSAFE_className="CommanderText">{commanders}</Text>
+                                    <Divider size="M" />
+                                    <Text UNSAFE_className="SaltText">Score: {salt}</Text>
+                                </Flex>
+                            </Flex>
+                            <Well alignSelf="center" width="100%">
+                                <Text UNSAFE_className="SaltText" alignSelf="center">{this.getSaltRating(salt)}</Text>
+                            </Well>
+                            <Flex direction="row" width="100%" marginTop="10px">
+                                <ActionButton 
+                                    onPress={this.handleDeckLinkPress}><LinkOut/>
+                                    Deck&nbsp;&nbsp;&nbsp;
+                                </ActionButton>
+                                <div style={{ width: '135px' }} />
+                                <ActionButton 
+                                    alignSelf="flex-end"
+                                    onPress={this.handleAuthorLinkPress}>
+                                        <LinkOut/>
+                                        Author Profile&nbsp;&nbsp;&nbsp;
+                                    </ActionButton>
+                            </Flex>
+                        </Flex>
+                    </div>
+                {/* </Dialog> */}
+            </DialogContainer>
         )
       }
 }
